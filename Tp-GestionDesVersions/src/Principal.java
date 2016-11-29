1ef96243e2612513ceeb823c31e4ec101fbdb854
yassern1 <yassern1@gmail.com>
2016-11-29 13:50:46 +0000

    First commit class principal

---
+++ Tp-GestionDesVersions/src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+	public Principal(){
+		printMessage();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Principal();
+	}
+	
+	public void printMessage(){
+		System.out.println("Bonjour Tout le Monde!!!!!");
+	}
+
+}
