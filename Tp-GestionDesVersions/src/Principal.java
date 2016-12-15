94fb98cb8e045c429fb6c9197f22c0e8fa676d77
yassern1 <yassern1@gmail.com>
2016-12-15 21:39:34 +0000

    classe client créée

--- Tp-GestionDesVersions/src/Principal.java
+++ Tp-GestionDesVersions/src/Principal.java
@@ -1,7 +1,13 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Principal {
 	public Principal(){
-		printMessage();
+		//printStartMessage();
+		printDateVol();
 	}
 
 	public static void main(String[] args) {
@@ -9,9 +15,25 @@ public class Principal {
 		new Principal();
 	}
 	
-	public void printMessage(){
+	public void printStartMessage(){
 		System.out.println("Bonjour Tout le Monde!!!!!");
 		System.out.println("premier commit!!!!");
+	
+	}
+	public void printDateVol(){
+		DateFormat dfm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			Date dep = dfm.parse("2016-11-28 20:15:00");
+			Date arri = dfm.parse("2016-11-29 08:00:00");
+			System.out.println("Date départ:"+dep);
+			System.out.println("Date arrivée:"+arri);
+
+		} catch (ParseException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
